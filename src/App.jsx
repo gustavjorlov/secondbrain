a79d01dc46f216612582640104d730ff73ff461d
gjorlov <gustav.jorlov@recordedfuture.com>
2025-01-09 14:43:28 +0100

    base name path

--- src/App.jsx
+++ src/App.jsx
@@ -23,17 +23,18 @@ const components = {
 };
 
 function App() {
+  const base = import.meta.env.PROD ? "/secondbrain" : "";
   return (
     <>
       <h1>...</h1>
       <MDXProvider components={components}>
-        <Router basename={import.meta.env.PROD ? "/secondbrain" : "/"}>
+        <Router basename={base}>
           <div className="container mx-auto px-4 py-8">
             <h1 className="text-3xl font-bold mb-8">Second Brain</h1>
             <Routes>
               <Route element={<h1>Nothing</h1>} />
-              <Route path="/" element={<Overview />} />
-              <Route path="/:filename" element={<Details />} />
+              <Route path={base + "/"} element={<Overview />} />
+              <Route path={base + "/:filename"} element={<Details />} />
             </Routes>
           </div>
         </Router>
